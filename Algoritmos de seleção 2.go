6ca9172b4e6b8fcdeda0fe2920744e03119a2de5
Miguel Artur de Castro Miranda <miguel.artur@dinfor.uniceub>
2023-03-20 10:58:01 -0300

    Algoritmo Seleção qst 3

--- Algoritmos de seleção 2.go
+++ Algoritmos de seleção 2.go
@@ -3,19 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	x := 3
-	y := 4
-	z := 5
+	var num int
+	fmt.Println("digite um número")
+	fmt.Scan(&num)
+	if num%2 == 0 {
+		fmt.Println("o número é par")
 
-	if x < y && x < z {
-		fmt.Println("x é menor que y e z")
-		if y < x && y < z {
-			fmt.Println("y é menor que x e z")
-			if z < x && z < y {
-				fmt.Println("z é menor que x e y")
-			}
-
-		}
+	} else {
+		fmt.Println("o número é ímpar")
 	}
 
 }
