605649fcf61fc77e448c7685360ca6095377428e
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:05:06 -0300

    Update ex 2 Seleção.go

--- ex 2 Seleção.go
+++ ex 2 Seleção.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var n uint32
-	fmt.Println("Insira um número:")
-	fmt.Scan(&n)
-	fmt.Println("Seu dobro é: ", n*2)
-	fmt.Println("Seu triplo é: ", n*3)
-	fmt.Println("Seu quadruplo é: ", n*4)
+	var n1, n2, n3 uint32
+	fmt.Println("Insira um número: ")
+	fmt.Scanln(&n1)
+	fmt.Println("Insira outro número: ")
+	fmt.Scanln(&n2)
+	fmt.Println("Insira mais um número: ")
+	fmt.Scanln(&n3)
+	if n1 < n2 && n1 < n3 {
+		fmt.Println(n1, "é o menor")
+	} else if n2 < n1 && n2 < n3 {
+		fmt.Println(n2, "é o menor")
+	} else if n3 < n1 && n3 < n2 {
+		fmt.Println(n3, "é o menor")
+	}
 }
